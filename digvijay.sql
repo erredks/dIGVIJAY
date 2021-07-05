5c940bb6a816737eb1c258899554bd4279a2215b
Digvijay <digvijayk723@gmail.com>
2021-07-05 15:00:22 +0530

    updated database

--- digvijay.sql
+++ digvijay.sql
@@ -48,7 +48,6 @@ INSERT INTO `user` (`sno`, `username`, `password`, `email`, `name`, `contact`, `
 (69, 'asif', 'ce0b996aa0b7d64169a4b8ffeaf878c5', 'abidsaleem04@gmail.com', 'asif', 8382841776, '323/412 South Murain Tola', '2021-06-26 20:32:22', '5ae779c94da8fda7c41da79ea69a5f', 'active'),
 (70, 'harshit', '83a75f0b31435193bafd3b9c5fd45aec', 'harshit@gmail.com', 'harshit', 8382841776, 'amroha', '2021-06-26 20:38:37', '6a720198d77b6cbd49a56efa35b6ce', 'active'),
 (71, 'akash', '94754d0abb89e4cf0a7f1c494dbb9d2c', 'akash@gmail.com', 'akash kumar', 7985628523, 'NOIDA', '2021-06-26 20:49:21', 'c0f1704b95e491821a72552e3e5474', 'active'),
-(74, 'oo', 'e47ca7a09cf6781e29634502345930a7', 'oo@gmail.com', 'oo', 7894561230, 'fsfssvdsv', '2021-06-26 22:22:15', 'de8985b44161a72ace60a48aff84fd', 'active'),
 (75, 'shreya', '4a3232c59ecda21ac71bebe3b329bf36', 'shreya@gmail.com', 'shreya', 5556987844, 'wfefe', '2021-06-27 00:09:37', '89c5314abcb996fea9f242599f0a67', 'active'),
 (76, 'digvijay', 'a589d839ad52ae2ee1f7f0fbd5287f8d', 'digvijayk723@gmail.com', 'Digvijay Kumar', 7985628523, '323/412 South Murain Tola', '2021-06-28 11:30:13', 'b7c0c7fab97c2d61d3d8d1e42e0c4f', 'active');
 
